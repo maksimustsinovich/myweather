ae16e70c27c95fec36c7f18dee64a6bee5dda21e
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2025-04-08 10:25:55 +0300

    ref: add weather conditions constants

---
+++ lib/consts/weather_conditions.dart
@@ -0,0 +1,70 @@
+class WeatherConstants {
+  // Group 2xx: Thunderstorm
+  static const int thunderstormLightRain = 200;
+  static const int thunderstormRain = 201;
+  static const int thunderstormHeavyRain = 202;
+  static const int lightThunderstorm = 210;
+  static const int thunderstorm = 211;
+  static const int heavyThunderstorm = 212;
+  static const int raggedThunderstorm = 221;
+  static const int thunderstormLightDrizzle = 230;
+  static const int thunderstormDrizzle = 231;
+  static const int thunderstormHeavyDrizzle = 232;
+
+  // Group 3xx: Drizzle
+  static const int lightIntensityDrizzle = 300;
+  static const int drizzle = 301;
+  static const int heavyIntensityDrizzle = 302;
+  static const int lightIntensityDrizzleRain = 310;
+  static const int drizzleRain = 311;
+  static const int heavyIntensityDrizzleRain = 312;
+  static const int showerRainAndDrizzle = 313;
+  static const int heavyShowerRainAndDrizzle = 314;
+  static const int showerDrizzle = 321;
+
+  // Group 5xx: Rain
+  static const int lightRain = 500;
+  static const int moderateRain = 501;
+  static const int heavyIntensityRain = 502;
+  static const int veryHeavyRain = 503;
+  static const int extremeRain = 504;
+  static const int freezingRain = 511;
+  static const int lightIntensityShowerRain = 520;
+  static const int showerRain = 521;
+  static const int heavyIntensityShowerRain = 522;
+  static const int raggedShowerRain = 531;
+
+  // Group 6xx: Snow
+  static const int lightSnow = 600;
+  static const int snow = 601;
+  static const int heavySnow = 602;
+  static const int sleet = 611;
+  static const int lightShowerSleet = 612;
+  static const int showerSleet = 613;
+  static const int lightRainAndSnow = 615;
+  static const int rainAndSnow = 616;
+  static const int lightShowerSnow = 620;
+  static const int showerSnow = 621;
+  static const int heavyShowerSnow = 622;
+
+  // Group 7xx: Atmosphere
+  static const int mist = 701;
+  static const int smoke = 711;
+  static const int haze = 721;
+  static const int sandDustWhirls = 731;
+  static const int fog = 741;
+  static const int sand = 751;
+  static const int dust = 761;
+  static const int volcanicAsh = 762;
+  static const int squalls = 771;
+  static const int tornado = 781;
+
+  // Group 800: Clear
+  static const int clearSky = 800;
+
+  // Group 80x: Clouds
+  static const int fewClouds = 801;
+  static const int scatteredClouds = 802;
+  static const int brokenClouds = 803;
+  static const int overcastClouds = 804;
+}
--- lib/helpers/weather_icon_helper.dart
+++ lib/helpers/weather_icon_helper.dart
@@ -1,72 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:myweather/consts/weather_conditions.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 class WeatherIconHelper {
   static IconData getWeatherIcon(int weatherId, bool isDaytime) {
     // Group 2xx: Thunderstorm
-    if (weatherId >= 200 && weatherId <= 232) {
+    if (weatherId >= WeatherConstants.thunderstormLightRain &&
+        weatherId <= WeatherConstants.thunderstormHeavyDrizzle) {
       return isDaytime
           ? WeatherIcons.day_thunderstorm
           : WeatherIcons.night_thunderstorm;
     }
 
     // Group 3xx: Drizzle
-    if (weatherId >= 300 && weatherId <= 321) {
+    if (weatherId >= WeatherConstants.lightIntensityDrizzle &&
+        weatherId <= WeatherConstants.showerDrizzle) {
       return isDaytime
           ? WeatherIcons.day_sprinkle
           : WeatherIcons.night_sprinkle;
     }
 
     // Group 5xx: Rain
-    if (weatherId == 500 || weatherId == 501) {
+    if (weatherId >= WeatherConstants.lightRain &&
+        weatherId <= WeatherConstants.moderateRain) {
       return isDaytime
           ? WeatherIcons.day_rain_mix
           : WeatherIcons.night_rain_mix;
     }
-    if (weatherId >= 502 && weatherId <= 504) {
+    if (weatherId >= WeatherConstants.heavyIntensityRain &&
+        weatherId <= WeatherConstants.extremeRain) {
       return isDaytime ? WeatherIcons.day_rain : WeatherIcons.night_rain;
     }
-    if (weatherId == 511) {
+    if (weatherId == WeatherConstants.freezingRain) {
       return WeatherIcons.rain_mix;
     }
-    if (weatherId >= 520 && weatherId <= 531) {
+    if (weatherId >= WeatherConstants.lightIntensityShowerRain &&
+        weatherId <= WeatherConstants.raggedShowerRain) {
       return isDaytime ? WeatherIcons.day_showers : WeatherIcons.night_showers;
     }
 
     // Group 6xx: Snow
-    if (weatherId >= 600 && weatherId <= 602) {
+    if (weatherId >= WeatherConstants.lightSnow &&
+        weatherId <= WeatherConstants.heavySnow) {
       return isDaytime ? WeatherIcons.day_snow : WeatherIcons.night_snow;
     }
-    if (weatherId >= 611 && weatherId <= 613) {
+    if (weatherId >= WeatherConstants.sleet &&
+        weatherId <= WeatherConstants.showerSleet) {
       return WeatherIcons.sleet;
     }
-    if (weatherId == 615 || weatherId == 616) {
+    if (weatherId >= WeatherConstants.lightRainAndSnow &&
+        weatherId <= WeatherConstants.rainAndSnow) {
       return WeatherIcons.rain_mix;
     }
-    if (weatherId >= 620 && weatherId <= 622) {
+    if (weatherId >= WeatherConstants.lightShowerSnow &&
+        weatherId <= WeatherConstants.heavyShowerSnow) {
       return isDaytime ? WeatherIcons.day_snow : WeatherIcons.night_snow;
     }
 
     // Group 7xx: Atmosphere
-    if (weatherId >= 701 && weatherId <= 781) {
+    if (weatherId >= WeatherConstants.mist &&
+        weatherId <= WeatherConstants.tornado) {
       return WeatherIcons.fog;
     }
 
     // Group 800: Clear
-    if (weatherId == 800) {
+    if (weatherId == WeatherConstants.clearSky) {
       return isDaytime ? WeatherIcons.day_sunny : WeatherIcons.night_clear;
     }
 
     // Group 80x: Clouds
-    if (weatherId == 801) {
+    if (weatherId == WeatherConstants.fewClouds) {
       return isDaytime ? WeatherIcons.day_cloudy : WeatherIcons.night_cloudy;
     }
-    if (weatherId == 802) {
+    if (weatherId == WeatherConstants.scatteredClouds) {
       return isDaytime
           ? WeatherIcons.day_cloudy_gusts
           : WeatherIcons.night_cloudy_gusts;
     }
-    if (weatherId == 803 || weatherId == 804) {
+    if (weatherId >= WeatherConstants.brokenClouds &&
+        weatherId <= WeatherConstants.overcastClouds) {
       return isDaytime ? WeatherIcons.cloudy : WeatherIcons.cloudy;
     }
 
